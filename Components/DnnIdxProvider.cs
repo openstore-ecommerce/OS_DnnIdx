0bf845cd1e842ca285624fcc778e8284eaf4c85a
Stefan Kamphuis <stefan@superska.net>
2021-07-28 08:28:32 +0200

    inintial commit

---
+++ Components/DnnIdxProvider.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Xml;
+using DotNetNuke.Application;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Services.Search;
+using DotNetNuke.Services.Search.Controllers;
+using DotNetNuke.Services.Search.Entities;
+using DotNetNuke.Services.Search.Internals;
+using NBrightCore.common;
+using NBrightDNN;
+using Nevoweb.DNN.NBrightBuy.Components;
+
+
+namespace Nevoweb.DNN.NBrightBuy.Providers
+{
+    /// <summary>
+    /// Implement DNN search Index
+    /// </summary>
+    public class DnnIdxProvider : Components.Interfaces.SchedulerInterface 
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="portalfinfo"></param>
+        /// <returns></returns>
+        public override string DoWork(int portalId)
+        {
+
+            try
+            {
+                var objCtrl = new NBrightBuyController();
+
+                var portal = PortalController.Instance.GetPortal(portalId);
+
+                // check if we have NBS in this portal by looking for default settings.
+                var nbssetting = objCtrl.GetByGuidKey(portalId, -1, "SETTINGS", "NBrightBuySettings");
+                if (nbssetting != null)
+                {
+                    var storeSettings = new StoreSettings(portalId);
+                    var pluginData = new PluginData(portalId); // get plugin data to see if this scheduler is active on this portal 
+                    var plugin = pluginData.GetPluginByCtrl("dnnsearchindex");
+                    if (plugin != null && plugin.GetXmlPropertyBool("genxml/interfaces/genxml[1]/checkbox/active"))
+                    {
+                        // The NBS scheduler is normally set to run hourly, therefore if we only want a process to run daily we need the logic this function.
+                        // To to this we keep a last run flag on the sceduler settings
+                        var setting = objCtrl.GetByGuidKey(portalId, -1, "DNNIDXSCHEDULER", "DNNIDXSCHEDULER");
+                        if (setting == null)
+                        {
+                            setting = new NBrightInfo(true);
+                            setting.ItemID = -1;
+                            setting.PortalId = portalId;
+                            setting.TypeCode = "DNNIDXSCHEDULER";
+                            setting.GUIDKey = "DNNIDXSCHEDULER";
+                            setting.ModuleId = -1;
+                            setting.XMLData = "<genxml></genxml>";
+                        }
+
+
+                        var lastrun = setting.GetXmlPropertyRaw("genxml/lastrun");
+                        var lastrundate = DateTime.Now.AddYears(-99);
+                        if (Utils.IsDate(lastrun)) lastrundate = Convert.ToDateTime(lastrun);
+
+                        var rtnmsg = DoProductIdx(portal, lastrundate, storeSettings.DebugMode);
+                        setting.SetXmlProperty("genxml/lastrun", DateTime.Now.ToString("s"), TypeCode.DateTime);
+                        objCtrl.Update(setting);
+                        if (rtnmsg != "") return rtnmsg;
+
+                    }
+                }
+
+                return " - OS-DNNIDX scheduler OK ";
+            }
+            catch (Exception ex)
+            {
+                return " - OS-DNNIDX scheduler FAIL: " + ex.ToString() + " : ";
+            }
+
+        }
+
+        private String DoProductIdx(DotNetNuke.Entities.Portals.PortalInfo portal, DateTime lastrun, Boolean debug)
+        {
+            if (debug)
+            {
+                InternalSearchController.Instance.DeleteAllDocuments(portal.PortalID, SearchHelper.Instance.GetSearchTypeByName("tab").SearchTypeId);
+            }
+            var searchDocs = new List<SearchDocument>();
+            var culturecodeList = DnnUtils.GetCultureCodeList(portal.PortalID);
+            var storeSettings = new StoreSettings(portal.PortalID);
+            foreach (var lang in culturecodeList)
+            {
+                var strContent = "";
+                // select all products
+                var objCtrl = new NBrightBuyController();
+                var strFilter = " and NB1.ModifiedDate > convert(datetime,'" + lastrun.ToString("s") + "') ";
+                if (debug) strFilter = "";
+                var l = objCtrl.GetList(portal.PortalID, -1, "PRD", strFilter);
+
+                foreach (var p in l)
+                {
+                    var prodData = new ProductData(p.ItemID, portal.PortalID, lang);
+
+                    strContent = prodData.Info.GetXmlProperty("genxml/textbox/txtproductref") + " : " + prodData.SEODescription + " " + prodData.SEOName + " " + prodData.SEOTitle;
+
+                    if (strContent != "")
+                    {
+                        var tags = new List<String>();
+                        tags.Add("osproduct");
+
+                        //Get the description string
+                        string strDescription = HtmlUtils.Shorten(HtmlUtils.Clean(strContent, false), 100, "...");
+                        var searchDoc = new SearchDocument();
+                        // Assigns as a Search key the SearchItems' 
+                        searchDoc.UniqueKey = prodData.Info.ItemID.ToString("");
+                        searchDoc.QueryString = "ref=" + prodData.Info.GetXmlProperty("genxml/textbox/txtproductref");
+                        searchDoc.Title = prodData.ProductName;
+                        searchDoc.Body = strContent;
+                        searchDoc.Description = strDescription;
+                        if (debug)
+                            searchDoc.ModifiedTimeUtc = DateTime.Now.Date;
+                        else
+                            searchDoc.ModifiedTimeUtc = prodData.Info.ModifiedDate;
+                        searchDoc.AuthorUserId = 1;
+                        searchDoc.TabId = storeSettings.ProductDetailTabId;
+                        searchDoc.PortalId = portal.PortalID;
+                        searchDoc.SearchTypeId = SearchHelper.Instance.GetSearchTypeByName("tab").SearchTypeId;
+                        searchDoc.CultureCode = lang;
+                        searchDoc.Tags = tags;
+                        //Add Module MetaData
+                        searchDoc.ModuleDefId = 0;
+                        searchDoc.ModuleId = 0;
+
+                        searchDocs.Add(searchDoc);
+                    }
+                }
+            }
+
+            //Index
+            InternalSearchController.Instance.AddSearchDocuments(searchDocs);
+            InternalSearchController.Instance.Commit();
+
+
+            return " - NBS-DNNIDX scheduler ACTIVATED ";
+
+        }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+OpenStore_DnnIdx Plugin
+-----------------------
+
+This DNN module is a plugin for OpenStore v4.  It enables products to be indexed into the standard DNN search database.
+
+It requires a minimum version of DNN9.7.1 and OpenStore 4.5.
+
+Installation
+------------
+
+- Install the OpenStore_DnnIdx_*.*.*_Install.zip as a normal DNN module, through the DNN extension.
+- Go into the NBS Back Office>Admin>Plugins. Edit and activate the "IBF Index DNN Search" plugin listed.
+- Ensure the NBS scheuler is running on the DNN scheduler. http://nbsdocs.nbrightproject.org/Documentation/Integratorguide/Setup.aspx
+
+Operation
+---------
+
+The OpenStore_DnnIdx plugin indexes products into the DNNsearch index by using the DNN scheudler.  The NBS scheduler module must be running on the DNN scheduler for the plugin to work.
+If the NBS store debug setting is turned on, the plugin will clear down ALL page indexes for the portal (Including all tabs loaded by other modules) and reindex only products.
+
+The DNN 9 index does tend to be very quick and in normal operation only changed products will be updated in the indexed, to avoid performace issues.
+
+Due to performace and restrictions with the the DNN search provider API the index process does not remove deleted products from the DNN index (unless in debug mode).
+To remove deleted product from the index you can either place the store in debug mode and run the OpenStore scheduler manually (or wait for the reindex) or use the DNN>Admin>Search-Admin option to reindex the search. 
+
+NOTE: The OpenStore module should not be left in debug mode during high peak hours.
+
+Search Type:  The OpenStore_DnnIdx uses a tab searchtype.
+Although each product is not a tab in DNN, the resulting detail is regarded as a tab with a uinque URL.
+For this reason we use SearchHelper.Instance.GetSearchTypeByName("tab").SearchTypeId
+
